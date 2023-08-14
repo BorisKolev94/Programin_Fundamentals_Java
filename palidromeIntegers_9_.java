151d7e861ca69fd1e38ca25cc387bd5ca966fa78
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-08-14 21:32:06 +0300

    Add files via upload

---
+++ palidromeIntegers_9_.java
@@ -0,0 +1,33 @@
+package methodsExercise;
+
+import java.util.Scanner;
+
+public class palidromeIntegers_9_ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("END")) {
+            System.out.println(isPalindrome(command));
+            command = scanner.nextLine();
+        }
+    }
+
+    //Метод, който провверява дали числото е палиндром -> true и false
+    private static boolean isPalindrome(String number) {
+        //проверка дали числото е същото, като числото записано наобратно
+        String reversedNumber = getReversedNumber(number);
+        return number.equals(reversedNumber);
+    }
+
+    //Метод, който взима обратното на числото
+    private static String getReversedNumber(String number) {
+        //обхождаме числото от последния към първия елемент
+        String reversed = "";
+        for (int index = number.length() - 1; index >= 0; index--) {
+            char currentElement = number.charAt(index);
+            reversed += currentElement;
+        }
+        return reversed;
+    }
+}
